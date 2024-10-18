c041765f6f9a9e511f6179c6b81b1ae255962c18
guilhermearaujodec <163536185+guilhermearaujodec@users.noreply.github.com>
2024-10-18 09:26:22 -0300

    Update Home.jsx

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const [temperatura, setTemperatura] =useState([])
 
   useEffect(()=>{
-    fetch("http://localhost:5000/temperatura")
+    fetch("https://bd-1-espg-one.vercel.app/")
     .then((res)=>{
         return res.json();
     })
